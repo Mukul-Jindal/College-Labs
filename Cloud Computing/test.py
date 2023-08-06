c3d0bfefcfb825817ec033d990e9677d353e51f8
Mukul-Jindal <mukul20jindal58@gmail.com>
2023-08-06 20:10:47 +0530

    1st commit

---
+++ Cloud Computing/test.py
@@ -0,0 +1,9 @@
+import random 
+import numpy as np
+import datetime
+print(datetime.time)
+rows = 65000
+cols=10000
+matrix = np.random.randint(0,10,size=(rows,cols),dtype=int)
+print(matrix)
+print(datetime.time)
