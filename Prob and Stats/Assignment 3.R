bb583dee7fd339a16b3a667e57fb2f1c333fea0e
Mukul Jindal <97662603+Mukul-Jindal@users.noreply.github.com>
2023-09-06 15:12:44 +0530

    prob Lab

--- Prob and Stats/Assignment 3.R
+++ Prob and Stats/Assignment 3.R
@@ -1,21 +1,36 @@
-# Q1
-prob7=dbinom(x = 7,prob = 1/6,size = 12)
-prob7
-prob8=dbinom(x = 8,prob = 1/6,size = 12)
-prob8
-prob9=dbinom(x = 9,prob = 1/6,size = 12)
-prob9
-prob7_9=pbinom(size = 12,prob = 1/6,q = 9)-pbinom(size = 12,prob = 1/6,q = 6)
-prob7_9
+# # Q1
+# prob7=dbinom(x = 7,prob = 1/6,size = 12)
+# prob7
+# prob8=dbinom(x = 8,prob = 1/6,size = 12)
+# prob8
+# prob9=dbinom(x = 9,prob = 1/6,size = 12)
+# prob9
+# prob7_9=pbinom(size = 12,prob = 1/6,q = 9)-pbinom(size = 12,prob = 1/6,q = 6)
+# prob7_9
 
-# Q2
-percentGreaterThanX=pnorm(q = 84,mean = 72,sd = 15.2,lower.tail = FALSE)
-percentGreaterThanX
+# #Q2
+# percentGreaterThanX=pnorm(q = 84,mean = 72,sd = 15.2,lower.tail = FALSE)
+# percentGreaterThanX
 
-# Q3
-lambda = 5
-probInOneHour = ppois(lambda = lambda,q = 0)
-probInOneHour
-lambda = lambda * 10 
-ans = ppois(q = 50,lambda = lambda) - ppois(q = 47,lambda = lambda)
-ans
+# # Q3
+# lambda = 5
+# probInOneHour = ppois(lambda = lambda,q = 0)
+# probInOneHour
+# lambda = lambda * 10 
+# ans = ppois(q = 50,lambda = lambda) - ppois(q = 47,lambda = lambda)
+# ans
+
+# # Q4
+# numberOfProcessors = 250
+# x=dhyper(3,n = 17,m = 233,k = 5)
+# print(x)
+
+# # Q5
+x = seq(0, 31)
+pmf=dbinom(x,31,0.447)
+plot(x,pmf)
+cdf = pbinom(x,31,0.447)
+plot(x, cdf)
+print(mean(x))
+print(var(x))
+print(sd(x))
---
+++ Prob and Stats/Assignment 4.R
@@ -0,0 +1,51 @@
+# # Q1
+# x=c(0,1,2,3,4)
+# probX = c(0.41,0.37,0.16,0.05,0.01)
+# print(x[3])
+# expected_Value = sum(x*probX)
+# print(expectedValue)
+
+# # Q2
+
+# expectedValue <- function (t){
+#     t*0.1*exp(-0.1*t)
+# }
+
+# integration123<- integrate(expectedValue,lower = 0,upper = Inf)
+# print(integration123$value)
+
+# # Q3
+# x = c(0,1,2,3)
+# probX = c(0.1,0.2,0.2,0.5)
+
+# y= 10 *x - 12 
+# expectedValue = sum(y * probX)
+# print(expectedValue)
+
+# # Q4
+# firstMoment1 <- function(t){t*0.5*exp(-1*abs(t))}
+# firstMoment2 <- function(t){t*t*0.5*exp(-1*abs(t))}
+# firstMoment <- integrate(firstMoment1,lower = 1,upper = 10)
+# secondMoment <- integrate(firstMoment2,lower = 1,upper = 10)
+# print(firstMoment$value)
+# print(secondMoment$value)
+
+# variance = secondMoment$value - (firstMoment$value*firstMoment$value)
+# print(variance)
+
+# Q5
+f<- function (y){(3/4)*(1/4)^(sqrt(y)-1)}
+x<-3
+y=x^2
+p<- f(y)
+print(p)
+
+f<-function(y){(3/4)*(1/4)^ (sqrt(y)-1)}
+x<-c(1,2,3,4,5)
+y<-x^2
+exp1 <- sum(y*f(y))
+print(exp1)
+
+exp2<-sum(y*y*f(y))
+var<- exp2 - (exp1)^2
+print(var)
