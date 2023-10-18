371247000eca4ad313978da71ae6ebcb5adc55d4
Mukul Jindal <97662603+Mukul-Jindal@users.noreply.github.com>
2023-10-18 15:22:53 +0530

    prob stats lab

--- Prob and Stats/Assignment 5.R
+++ Prob and Stats/Assignment 5.R
@@ -8,4 +8,28 @@ b = punif(30,0,60) - punif(19,0,60)
 b
 
 #Q2 A
-a = 
+a = dexp(x = 3,rate = 1/2)
+a
+#Q2 B
+x<- seq(0,5,0.04)
+x
+y<- dexp(x,1/2)
+plot(x = x,y = y)
+#Q2 C
+c = pexp(q = 3,rate = 1/2)
+c
+#Q2 D
+y<-pexp(q = x,rate = 1/2)
+plot(x,y)
+#Q2 E
+n=1000
+sim = rexp(n,1/2)
+plot(density(sim))
+
+#Q3 A
+alpha = 2
+beta = 1/3
+a=pgamma(1,shape = alpha, scale=beta,lower.tail = FALSE)
+a
+b= qgamma(0.70,shape=alpha,scale = beta)
+b
---
+++ Prob and Stats/Assignment 6.R
@@ -0,0 +1,73 @@
+install.packages("pracman")
+library('pracma')
+
+#Q1 A
+f <- function(x,y){ 2*(2*x+3*y)/5}
+i = integral2(f,xmin = 0,xmax = 1,ymin = 0,ymax = 1)
+i$Q
+
+#Q1 B
+
+gx= function(y){f(1,y)}
+gxi = integral(gx,xmin = 0,xmax = 1)
+gxi
+
+#Q1 C
+
+hy = function(x){f(x,0)}
+hyi = integral(hy,xmin = 0,xmax = 1)
+hyi
+
+#Q1 D
+
+gxy = function(x,y){x*y*f(x,y)}
+gxyi = integral2(gxy,xmin = 0,xmax = 1,ymin = 0,ymax = 1)
+gxyi$Q
+
+
+#Q2 
+f = function(x,y){(x+y)/30}
+x= c(0:3)
+y=c(0:2)
+
+#A
+m = matrix(c(f(0,0:2),f(1,0:2),f(2,0:2),f(3,0:2)),nrow = 4,ncol = 3,byrow = TRUE)
+m
+
+#B
+ans = sum(m)
+if(ans == 1){
+  print("JMF")
+} else
+{
+  print("Not JMF")
+}
+
+#c
+marginalx = apply(m,MARGIN = 1,FUN = sum)
+marginalx
+
+#d
+marginaly = apply(m,MARGIN = 2,FUN = sum)
+marginaly
+
+#e
+m[1,2]/marginaly[2]
+
+#f
+ex = sum(x*marginalx)
+ex
+ey = sum(y*marginaly)
+ey
+
+exx = sum(x*x*marginalx)
+variancex = exx - ex**2
+variancex
+
+eyy = sum(y*y*marginaly)
+variancey = eyy - ey**2
+variancey
+
+
+exy = sum(x*y*f(x,y))
+exy
